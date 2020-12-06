648b6b5212ab1a399bb911a7e8d0e59da29de934
Dani JM <xdanielmtz@gmail.com>
2020-12-05 20:59:43 -0500

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (name) VALUES ('Bacnonator');
 INSERT INTO burgers (name) VALUES ('Big Mac');
-INSERT INTO burgers (name, sleepy) VALUES ('Whopper', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Bacon King', true);
+INSERT INTO burgers (name, eaten) VALUES ('Whopper', true);
+INSERT INTO burgers (name, eaten) VALUES ('Bacon King', true);
