6f3306c029b1467f648926ab836fb26dd98eac73
Dani JM <xdanielmtz@gmail.com>
2020-12-07 14:49:03 -0500

    final changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO burgers (name) VALUES ('Molly');
-INSERT INTO burgers (name) VALUES ('Charlie');
-INSERT INTO burgers (name, eaten) VALUES ('Poppy', true);
-INSERT INTO burgers (name, eaten) VALUES ('Oscar', true);
-INSERT INTO burgers (name, eaten) VALUES ('Smudge', true);
-INSERT INTO burgers (name) VALUES ('Daisy');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('BBQ Burger', true);
